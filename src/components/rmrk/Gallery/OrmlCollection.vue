80fbcb02ffacc862e6ef13711fda018b09b0d25a
Viki Val <viktorko99@gmail.com>
2021-09-17 15:48:21 +0200

    New collection

--- src/components/rmrk/Gallery/OrmlCollection.vue
+++ src/components/rmrk/Gallery/OrmlCollection.vue
@@ -1,21 +1,37 @@
 <template>
   <div class="pack-item-wrapper container">
+    <div class="columns is-centered">
+      <div class="column is-half has-text-centered">
+        <figure class="image container is-128x128">
+          <img class="is-rounded" :src="image">
+        </figure>
+        <h1 class="title is-2 mt-2">
+          {{ name }}
+        </h1>
+      </div>
+    </div>
+
     <div class="columns">
       <div class="column">
-        <h1 class="title is-4">Collection {{ name }}</h1>
-      </div>
-      <div class="column">
-        <p v-if="issuer" class="subtitle">
-          Creator
-          <ProfileLink :address="issuer" :inline="true" :showTwitter="true" />
+        <p class="subtitle">
+          Creator <ProfileLink :address="issuer" :inline="true" :showTwitter="true"/>
+        </p>
+        <p class="subtitle" v-if="owner">
+          Owner <ProfileLink :address="owner" :inline="true" />
         </p>
       </div>
       <div class="column is-2">
-        <Sharing
-          v-if="sharingVisible"
+        <Sharing v-if="sharingVisible"
           label="Check this awesome Collection on %23KusamaNetwork %23KodaDot"
-          :iframe="iframeSettings"
-        />
+          :iframe="iframeSettings" />
+      </div>
+    </div>
+
+    <div class="columns is-centered">
+      <div class="column is-8 has-text-centered">
+        <p class="content">
+          {{ description }}
+        </p>
       </div>
     </div>
 
@@ -24,6 +40,7 @@
       type="superDetail"
       :collection="id"
       :formatId="formater"
+      class="mb-2"
     />
   </div>
 </template>
@@ -61,16 +78,28 @@ export default class OrmlCollection extends Vue {
   private isLoading: boolean = false;
   private formater = tokenIdToRoute;
 
-  get name() {
-    return this.collection.name || this.id;
+  get image() {
+    return this.collection.image || '/koda300x300.svg'
+  }
+
+  get description() {
+    return this.collection.description || ''
+  }
+
+	get name() {
+    return this.collection.name || this.id
   }
 
   get issuer() {
-    return this.collection.issuer || '';
+    return this.collection.issuer || ''
+  }
+
+  get owner() {
+    return this.collection.issuer === (this.collection as any).currentOwner ? '' : (this.collection as any).currentOwner
   }
 
   get sharingVisible() {
-    return !isShareMode;
+    return !isShareMode
   }
 
   public created() {
